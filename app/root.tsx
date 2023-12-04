09fb059e975b0e0ab8be5fc7479cbac6b302c12f
George Moller <jmoller2106@gmail.com>
2023-12-03 22:12:07 -0300

    Add meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,40 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Master React</title>
+        <meta name="title" content="Master React" />
+        <meta
+          name="description"
+          content="Empower Your Frontend Journey with 100+ Insightful Infographics for Crafting Exceptional Web Experiences!"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="react-infographics.georgemoller.com" />
+        <meta property="og:title" content="Master React" />
+        <meta
+          property="og:description"
+          content="Empower Your Frontend Journey with 100+ Insightful Infographics and 70+ video tutorials for Crafting Exceptional Web Experiences!"
+        />
+
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dptgkdbjg/image/upload/v1701652135/Frame_87_2_px9eki.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="master-react.georgemoller.com"
+        />
+        <meta property="twitter:title" content="100+ React Infographics" />
+        <meta
+          property="twitter:description"
+          content="Empower Your Frontend Journey with 100+ Insightful Infographics for Crafting Exceptional Web Experiences!"
+        />
+        <meta
+          property="twitter:image"
+          content="https://res.cloudinary.com/dptgkdbjg/image/upload/v1701652135/Frame_87_2_px9eki.png"
+        />
         <Meta />
         <Links />
       </head>
